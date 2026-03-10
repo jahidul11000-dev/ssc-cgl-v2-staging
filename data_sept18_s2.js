b378d67537fb168b140cde5b028c4f5c10a3179f
jahidul11000-dev <jahidul9547872638@gmail.com>
2026-03-10 11:49:32 +0530

    Add files via upload

---
+++ data_sept18_s2.js
@@ -0,0 +1,124 @@
+// Auto-Generated Mock Test File
+
+if(typeof mockDatabase === 'undefined') { var mockDatabase = {}; }
+
+Object.assign(mockDatabase, {
+    "Sept18_S2": {
+        "General Intelligence": [
+            {
+                "qNo": 1,
+                "text": "AZ, BY, CX, ?",
+                "img": "",
+                "options": {
+                    "A": "DV",
+                    "B": "DW",
+                    "C": "WD",
+                    "D": "VD"
+                },
+                "correct": "B"
+            }
+        ],
+        "General Awareness": [
+            {
+                "qNo": 1,
+                "text": "Who is the first prime minister of India ?",
+                "img": "",
+                "options": {
+                    "A": "Pandit Jawaharlal Nehru",
+                    "B": "Mahatma Gandhi",
+                    "C": "Dr Rajendra Prasad",
+                    "D": "Dr Sarvapalli Radhakrishnan"
+                },
+                "correct": "B"
+            },
+            {
+                "qNo": 2,
+                "text": "What is the national animal of India ?",
+                "img": "",
+                "options": {
+                    "A": "Peacock",
+                    "B": "Lion",
+                    "C": "Tiger",
+                    "D": "Hornbill"
+                },
+                "correct": "C"
+            },
+            {
+                "qNo": 3,
+                "text": "Who is widely known as 'God Of Cricket' ?",
+                "img": "",
+                "options": {
+                    "A": "Sachin Tendulkar",
+                    "B": "Virat Kohli",
+                    "C": "Mahendra Singh Dhoni",
+                    "D": "Rohit Sharma"
+                },
+                "correct": "A"
+            },
+            {
+                "qNo": 4,
+                "text": "Simplify :\n154-36+285÷57",
+                "img": "",
+                "options": {
+                    "A": "121",
+                    "B": "123",
+                    "C": "133",
+                    "D": "213"
+                },
+                "correct": "B"
+            }
+        ],
+        "Quantitative Aptitude": [
+            {
+                "qNo": 1,
+                "text": "A man lends a certain amount of money at 4% per annum rate of compound interest for 2 years. If the interest earned by him is rupees 1632, what was the amount lend ?",
+                "img": "",
+                "options": {
+                    "A": "2000",
+                    "B": "20000",
+                    "C": "10010",
+                    "D": "20900"
+                },
+                "correct": "B"
+            },
+            {
+                "qNo": 2,
+                "text": "He failed to hit _______ target.",
+                "img": "",
+                "options": {
+                    "A": "a",
+                    "B": "the",
+                    "C": "an",
+                    "D": "No article"
+                },
+                "correct": "B"
+            }
+        ],
+        "English Comprehension": [
+            {
+                "qNo": 1,
+                "text": "Which one of the following words is INCORRECTLY spelt ?",
+                "img": "",
+                "options": {
+                    "A": "driver ",
+                    "B": "Safty",
+                    "C": "Hibernation",
+                    "D": "Iteration"
+                },
+                "correct": "B"
+            },
+            {
+                "qNo": 2,
+                "text": "India : New Delhi :: Bangladesh : ?",
+                "img": "",
+                "options": {
+                    "A": "Dhaka",
+                    "B": "Kumilla",
+                    "C": "Barishal",
+                    "D": "Rajshahi"
+                },
+                "correct": "A"
+            }
+        ]
+    }
+});
