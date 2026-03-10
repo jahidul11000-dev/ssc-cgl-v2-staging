b97840f57aeb14848c6ed6cbc77ad578711daf19
jahidul11000-dev <jahidul9547872638@gmail.com>
2026-03-10 16:27:58 +0530

    Add files via upload

--- data_sept18_s2.js
+++ data_sept18_s2.js
@@ -7,131 +7,20 @@ Object.assign(mockDatabase, {
         "General Intelligence": [
             {
                 "qNo": 1,
-                "text": "AZ, BY, CX, ?",
+                "text": "In the following question, select the related word from the given alternatives\n<b>Liberty: Autonomy:: Equality: ?</b>",
                 "img": "",
                 "options": {
-                    "A": "DV",
-                    "B": "DW",
-                    "C": "WD",
-                    "D": "VD"
-                },
-                "correct": "B"
-            }
-        ],
-        "General Awareness": [
-            {
-                "qNo": 1,
-                "text": "Who is the first prime minister of India ?",
-                "img": "",
-                "options": {
-                    "A": "Pandit Jawaharlal Nehru",
-                    "B": "Mahatma Gandhi",
-                    "C": "Dr Rajendra Prasad",
-                    "D": "Dr Sarvapalli Radhakrishnan"
-                },
-                "correct": "B"
-            },
-            {
-                "qNo": 2,
-                "text": "What is the national animal of India ?",
-                "img": "",
-                "options": {
-                    "A": "Peacock",
-                    "B": "Lion",
-                    "C": "Tiger",
-                    "D": "Hornbill"
-                },
-                "correct": "C"
-            },
-            {
-                "qNo": 3,
-                "text": "Who is widely known as 'God Of Cricket' ?",
-                "img": "",
-                "options": {
-                    "A": "Sachin Tendulkar",
-                    "B": "Virat Kohli",
-                    "C": "Mahendra Singh Dhoni",
-                    "D": "Rohit Sharma"
-                },
-                "correct": "A"
-            },
-            {
-                "qNo": 4,
-                "text": "Simplify :\n154-36+285÷57",
-                "img": "",
-                "options": {
-                    "A": "121",
-                    "B": "123",
-                    "C": "133",
-                    "D": "213"
-                },
-                "correct": "B"
-            }
-        ],
-        "Quantitative Aptitude": [
-            {
-                "qNo": 1,
-                "text": "A man lends a certain amount of money at 4% per annum rate of compound interest for 2 years. If the interest earned by him is rupees 1632, what was the amount lend ?",
-                "img": "",
-                "options": {
-                    "A": "2000",
-                    "B": "20000",
-                    "C": "10010",
-                    "D": "20900"
-                },
-                "correct": "B"
-            },
-            {
-                "qNo": 2,
-                "text": "He failed to hit _______ target.",
-                "img": "",
-                "options": {
-                    "A": "a",
-                    "B": "the",
-                    "C": "an",
-                    "D": "No article"
-                },
-                "correct": "B"
-            },
-            {
-                "qNo": 3,
-                "text": "Solve for $x$: $2x + \\sqrt{16} = 10$",
-                "img": "",
-                "options": {
-                    "A": "2",
-                    "B": "5",
-                    "C": "3",
-                    "D": "4"
+                    "A": "Legitimacy",
+                    "B": "Neutrality",
+                    "C": "Fairness",
+                    "D": "Diversity"
                 },
                 "correct": "C",
-                "explanation": "Since $\\sqrt{16} = 4$, we have $2x + 4 = 10 \\implies 2x = 6 \\implies x = 3$."
+                "explanation": ""
             }
         ],
-        "English Comprehension": [
-            {
-                "qNo": 1,
-                "text": "Which one of the following words is INCORRECTLY spelt ?",
-                "img": "",
-                "options": {
-                    "A": "driver ",
-                    "B": "Safty",
-                    "C": "Hibernation",
-                    "D": "Iteration"
-                },
-                "correct": "B"
-            },
-            {
-                "qNo": 2,
-                "text": "India : New Delhi :: Bangladesh : ?",
-                "img": "",
-                "options": {
-                    "A": "Dhaka",
-                    "B": "Kumilla",
-                    "C": "Barishal",
-                    "D": "Rajshahi"
-                },
-                "correct": "A"
-            }
-        ]
+        "General Awareness": [],
+        "Quantitative Aptitude": [],
+        "English Comprehension": []
     }
 });
