fdea41f18a34ee387bdd9c069edfbce0c293871e
jahidul11000-dev <jahidul9547872638@gmail.com>
2026-03-10 11:01:24 +0530

    Add files via upload

---
+++ mock_data.js
@@ -0,0 +1,54 @@
+// This is your Master Database of all Mock Questions
+
+const mockDatabase = {
+
+    // --- MOCK 1: Sept 18, Shift 2 ---
+    "Sept18_S2": {
+        "General Intelligence": [
+            {
+                qNo: 1,
+                text: "Select the figure that will come next in the following figure series.",
+                img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Icecream.svg/200px-Icecream.svg.png", // Leave blank like "" if no image
+                options: {
+                    "A": "Figure 1",
+                    "B": "Figure 2",
+                    "C": "Figure 3",
+                    "D": "Figure 4"
+                },
+                correct: "B"
+            },
+            {
+                qNo: 2,
+                text: "If 'A + B' means 'A is the father of B', and 'A * B' means 'A is the brother of B', what does 'P + Q * R' mean?",
+                img: "", // No image for this question
+                options: {
+                    "A": "P is the uncle of R",
+                    "B": "P is the father of R",
+                    "C": "P is the brother of R",
+                    "D": "P is the grandfather of R"
+                },
+                correct: "B"
+            }
+            // ... You would paste questions 3 through 25 here
+        ],
+
+        "General Awareness": [
+            // ... 25 Questions here
+        ],
+
+        "Quantitative Aptitude": [
+            // ... 25 Questions here
+        ],
+
+        "English Comprehension": [
+            // ... 25 Questions here
+        ]
+    },
+
+
+    // --- MOCK 2: Sept 12, Shift 3 ---
+    "Sept12_S3": {
+        // ... The structure repeats for the next mock
+    }
+
+};
